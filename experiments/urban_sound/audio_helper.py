922b2b631eae25c85a202c1d4b72d4c6a7bb63cb
Chandan Singh <chandan_singh@berkeley.edu>
2020-01-03 18:23:34 -0800

    trained net + transform

--- experiments/urban_sound/audio_helper.py
+++ experiments/urban_sound/audio_helper.py
@@ -36,7 +36,7 @@ class UrbanSoundDataset(Dataset):
                 self.folders.append(csvData.iloc[i, 5])
                 
         self.file_path = file_path
-        self.mixer = torchaudio.transforms.DownmixMono() #UrbanSound8K uses two channels, this will convert them to one
+        # self.mixer = torchaudio.transforms.DownmixMono() #UrbanSound8K uses two channels, this will convert them to one
         self.folderList = folderList
         
     def __getitem__(self, index):
@@ -44,7 +44,9 @@ class UrbanSoundDataset(Dataset):
         path = self.file_path + "fold" + str(self.folders[index]) + "/" + self.file_names[index]
         sound = torchaudio.load(path, out = None, normalization = True)
         #load returns a tensor with the sound data and the sampling frequency (44.1kHz for UrbanSound8K)
-        soundData = self.mixer(sound[0])
+        # soundData = self.mixer(sound[0])
+#         print(sound[0].shape)
+        soundData = sound[0].mean(axis=0).reshape(-1, 1) #UrbanSound8K uses two channels, this will convert them to one
         #downsample the audio to ~8kHz
         tempData = torch.zeros([160000, 1]) #tempData accounts for audio clips that are too short
         if soundData.numel() < 160000:
@@ -60,3 +62,38 @@ class UrbanSoundDataset(Dataset):
     
     def __len__(self):
         return len(self.file_names)
+
+def train(model, epoch, optimizer, train_loader, device, log_freq=10):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        data = data.to(device)
+        target = target.to(device)
+        data = data.requires_grad_() #set requires_grad to True for training
+        output = model(data) # output is (batch_size, num_classes)
+        # print('shapes', data.shape, output.shape, target.shape)
+        # output = output.permute(1, 0, 2) #original output dimensions are batchSizex1x10 
+        loss = F.nll_loss(output, target) #the loss functions expects a batchSizex10 input
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_freq == 0: # print training stats
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss))
+            
+            
+def test(model, test_loader, device):
+    model.eval()
+    correct = 0
+    for data, target in test_loader:
+        data = data.to(device)
+        target = target.to(device)
+        output = model(data) # output is (batch_size, num_classes)
+        preds = torch.argmax(output, dim=1)
+        # output = output.permute(1, 0, 2)
+        # pred = output.max(2)[1] # get the index of the max log-probability
+        correct += (preds == target).cpu().sum().item()
+    acc = 100. * correct / len(test_loader.dataset)
+    print('\nTest set: Accuracy: {}/{} ({:.0f}%)\n'.format(
+        correct, len(test_loader.dataset), acc ))
+    return acc
