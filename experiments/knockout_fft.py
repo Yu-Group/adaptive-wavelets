81076da55854be737d06ae6cbf61cb35c9b05282
Ubuntu <ubuntu@ip-172-31-48-44.us-west-2.compute.internal>
2020-02-13 17:32:59 +0000

    update batchfft

--- experiments/knockout_fft.py
+++ experiments/knockout_fft.py
@@ -1,24 +1,99 @@
 import numpy as np
 import torch
 import random
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 import os, sys
 opj = os.path.join
+from tqdm import tqdm
+from functools import partial
 import acd
 from copy import deepcopy
 sys.path.append('..')
-sys.path.append('../../dsets/mnist')
+from transforms_torch import bandpass_filter, bandpass_filter_augment, batch_fftshift2d, batch_ifftshift2d
+import transform_wrappers
+sys.path.append('../dsets/mnist')
 import dset
 from model import Net, Net2c
 from util import *
-from numpy.fft import *
 from torch import nn
-import pickle as pkl
-from transform_wrappers import *
-from captum.attr import *
-sys.path.append('../..')
-from acd_wooseok.acd.scores import cd, score_funcs
+from style import *
 
 
+def train_Net2c(train_loader, args, band_center=0.3, band_width=0.1, save_path='models/cnn_vanilla.pth'):
+    # seed
+    random.seed(13)
+    np.random.seed(13)
+    torch.manual_seed(13)
+    if device == 'cuda':
+        torch.cuda.manual_seed(13)
+
+    # model
+    model = Net2c().to(device)
+
+    # optimizer
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    # train
+    for epoch in range(1, args.epochs + 1):
+        model.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            B = len(target)
+            data = bandpass_filter_augment(data, band_center, band_width).to(device)
+            target = torch.ones(2*B, dtype=target.dtype).to(device)
+            target[B:] = 0
+            # zero grad
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % args.log_interval == 0:
+                print('\rTrain Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * 2*B, 2*len(train_loader.dataset),
+                           100. * batch_idx / len(train_loader), loss.data.item()), end='')
+
+    torch.save(model.state_dict(), save_path)
+    
+
+# test Net2c
+def test_Net2c(test_loader, model, band_center=0.3, band_width=0.1):
+    # eval mode
+    model = model.to(device)
+    model.eval()
+
+    # test
+    test_loss = 0
+    correct = 0
+    for batch_idx, (data, target) in tqdm(enumerate(test_loader)):
+        B = len(target)
+        data = bandpass_filter_augment(data, band_center, band_width).to(device)
+        target = torch.ones(2*B, dtype=target.dtype).to(device)
+        target[B:] = 0
+        output = model(data)
+        test_loss += F.nll_loss(output, target, reduction='sum').data.item()  # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= 2*len(test_loader.dataset)
+    return test_loss, correct          
+        
+    
+
+def test_knockout_models(test_loader, args, true_band_centers, device='cuda'):
+    true_band_num = len(true_band_centers)
+    for i in range(true_band_num):
+        # load model
+        model = Net2c().to(device)
+        model.load_state_dict(torch.load(opj('models/freq','net2c_' + str(i) + '.pth'), map_location=device))
+        model = model.eval()
+
+        # test accuracy of the model
+        test_loss, correct = test_Net2c(test_loader, args, model, true_band_centers[i], band_width=0.1)
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+            test_loss, correct, 2*len(test_loader.dataset),
+            100. * correct / (2*len(test_loader.dataset))))
+        
+        
 def get_attributions(x_t: torch.Tensor, mt, class_num=0, device='cuda'):
     '''Returns all scores in a dict assuming mt works with both grads + CD
 
@@ -54,44 +129,27 @@ def get_attributions(x_t: torch.Tensor, mt, class_num=0, device='cuda'):
             if x_t.shape[-1] == 2: # check for imaginary representations
                 attributions = mag(attributions)
         results[name] = attributions
-    return results
+    return results        
 
 
-# test Net2c
-def test_Net2c(test_loader, args, model, band_center=0.3, band_width=0.1):
-    # eval mode
-    model.eval()
-    if args.cuda:
-        model.cuda()
+if __name__ == '__main__':
+    # set args
+    args = dset.get_args()
 
-    # test
-    test_loss = 0
-    correct = 0
-    for batch_idx, (data, target) in tqdm(enumerate(test_loader)):
-        data = bandpass_filter_augment(data, band_center, band_width)
-        target = torch.zeros(2*args.test_batch_size, dtype=target.dtype)
-        target[args.test_batch_size:] = 1
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        output = model(data)
-        test_loss += F.nll_loss(output, target, reduction='sum').data.item()  # sum up batch loss
-        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+    # load mnist data
+    train_loader, test_loader = dset.load_data(args.batch_size, args.test_batch_size, device)
+    
+    # freq band
+    band_center = 0.3
+    band_width = 0.05
 
-    test_loss /= 2*len(test_loader.dataset)
-    return test_loss, correct
-
-
-def test_knockout_models(test_loader, args, true_band_centers, device='cuda'):
-    true_band_num = len(true_band_centers)
-    for i in range(true_band_num):
-        # load model
-        model = Net2c().to(device)
-        model.load_state_dict(torch.load(opj('models/freq','net2c_' + str(i) + '.pth'), map_location=device))
-        model = model.eval()
-
-        # test accuracy of the model
-        test_loss, correct = test_Net2c(test_loader, args, model, true_band_centers[i], band_width=0.1)
-        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
-            test_loss, correct, 2*len(test_loader.dataset),
-            100. * correct / (2*len(test_loader.dataset))))
+    # train model
+    train_Net2c(train_loader, args, band_center, band_width, save_path=opj('mnist/models/freq','net2c_' + str(0) + '.pth'))
+    model = Net2c().to(device)
+    model.load_state_dict(torch.load(opj('mnist/models/freq','net2c_' + str(0) + '.pth'), map_location=device))
+    
+    # test model
+    test_loss, correct = test_Net2c(test_loader, model, band_center, band_width)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+        test_loss, correct, 2*len(test_loader.dataset),
+        100. * correct / (2*len(test_loader.dataset))))  
--- experiments/transforms_torch.py
+++ experiments/transforms_torch.py
@@ -1,8 +1,31 @@
 import transforms_np
 import torch
+import numpy as np
+from numpy.fft import *
 from copy import deepcopy
 
-def bandpass_filter(im: torch.Tensor, band_center=0.3, band_width=0.1, sample_spacing=None, mask=None):
+# def bandpass_filter(im: torch.Tensor, band_center=0.3, band_width=0.1, sample_spacing=None, mask=None):
+#    '''Bandpass filter the image (assumes the image is square)
+#
+#    Returns
+#    -------
+#    im_bandpass: torch.Tensor
+#        H, W
+#    '''
+#    im_np = im.squeeze().cpu().detach().numpy()
+#    if mask is None:
+#        im_bandpass = transforms_np.bandpass_filter_norm_fast(im_np,
+#                                                              cutoff_low=band_center - band_width / 2,
+#                                                              cutoff_high=band_center + band_width / 2,
+#                                                              kernel_length=25)
+#    else:
+#        im_bandpass = transforms_np.bandpass_filter(im_np, band_center, band_width, sample_spacing, mask=mask)
+#
+#
+#    return torch.Tensor(im_bandpass).reshape(1, 1, im_np.shape[0], im_np.shape[1])
+
+
+def bandpass_filter(im: torch.Tensor, band_center=0.3, band_width=0.1):
     '''Bandpass filter the image (assumes the image is square)
 
     Returns
@@ -10,34 +33,33 @@ def bandpass_filter(im: torch.Tensor, band_center=0.3, band_width=0.1, sample_sp
     im_bandpass: torch.Tensor
         H, W
     '''
-    im_np = im.squeeze().cpu().detach().numpy()
-    if mask is None:
-        im_bandpass = transforms_np.bandpass_filter_norm_fast(im_np,
-                                                              cutoff_low=band_center - band_width / 2,
-                                                              cutoff_high=band_center + band_width / 2,
-                                                              kernel_length=25)
-    else:
-        im_bandpass = transforms_np.bandpass_filter(im_np, band_center, band_width, sample_spacing, mask=mask)
+    freq_arr = fftshift(fftfreq(n=im.shape[-1]))
+    freq_arr /= np.max(np.abs(freq_arr))
 
+    im_f = batch_fftshift2d(torch.rfft(im, 2, onesided=False))    
+    mask_bandpass = torch.zeros(im_f.shape)
 
-    return torch.Tensor(im_bandpass).reshape(1, 1, im_np.shape[0], im_np.shape[1])
+    for r in range(im_f.shape[2]):
+        for c in range(im_f.shape[3]):
+            dist = np.sqrt(freq_arr[r]**2 + freq_arr[c]**2)
+            if dist > band_center - band_width / 2 and dist < band_center + band_width / 2:
+                mask_bandpass[:, :, r, c, :] = 1
+    im_f_masked = torch.mul(im_f, mask_bandpass)
+    im_bandpass = torch.irfft(batch_ifftshift2d(im_f_masked), 2, onesided=False)
 
+    return im_bandpass
+    
 
-
-
-def bandpass_filter_augment(im: torch.Tensor, band_center=0.3, band_width=0.1, sample_spacing=None, mask=None):
+def bandpass_filter_augment(im: torch.Tensor, band_center=0.3, band_width=0.1):
     '''
     Returns
     -------
     im: torch.Tensor
         B, H, W
     '''
-    batch_size = im.size()[0]
     im_copy = deepcopy(im)
-    for i in range(batch_size):
-        im_bandpass = bandpass_filter(im[i], band_center, band_width, sample_spacing, mask)
-        im_copy = torch.cat((im_copy,im[i]-im_bandpass), dim=0)
-    return im_copy
+    im_p = im_copy - bandpass_filter(im, band_center, band_width)
+    return torch.cat((im_copy,im_p), dim=0)  
 
 
 def perturb_wt(im: torch.Tensor, t, transform_i, idx=2, p=0.5):
@@ -52,6 +74,7 @@ def perturb_wt(im: torch.Tensor, t, transform_i, idx=2, p=0.5):
     im_t = t(im)
     mask = torch.bernoulli((1-p) * torch.ones(im.shape[0], 5, 5))
     im_t[1][0][:,0,idx,6:11,6:11] = im_t[1][0][:,0,idx,6:11,6:11] * mask
+    # im_t[1][0][:,0,idx,6:11,6:11] = 0
     return transform_i(im_t)
 
 
