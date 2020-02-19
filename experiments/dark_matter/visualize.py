14d7fa4d2fe6742b364e838e9096c8c1071336b1
Chandan Singh <chandan_singh@berkeley.edu>
2020-02-18 20:40:09 -0800

    add paper figs

--- experiments/dark_matter/visualize.py
+++ experiments/dark_matter/visualize.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from style import *
+
 
 def visualize(im_orig, transform):
     plt.figure(dpi=100, figsize=(9, 6))
@@ -26,3 +28,28 @@ def visualize(im_orig, transform):
 def cshow(im):
     plt.imshow(im, cmap='magma', vmax=0.15, vmin=-0.05)
     plt.axis('off')
+    
+    
+def plot_all(bs, scores_list, preds_list, params_list, class_num=1, param_num=None, tit=None, ylab=True):
+    s = scores_list[..., class_num].T / preds_list[:, class_num] # (num_bands, num_curves)
+    
+    if param_num is not None:
+        if tit is None:
+            s = s[:, param_num::len(params_list)] # skip every 10 images
+            params = params_list[param_num]
+            plt.title(f"$m_\\nu=${params[0]:0.2f}   $\Omega_m$={params[1]:0.2f}    $10^9A_s$={params[2]:0.2f}")
+#         plt.title(str())
+        else:
+            plt.title(tit)
+    else:
+        if tit is None:
+            plt.title('all params')
+        else:
+            plt.title(tit)
+    
+#     print(bs)
+    plt.plot(bs, np.array(s), '-', alpha=0.1, color=cb)
+    plt.plot(bs, np.array(s).mean(axis=1), '-', color=cr)
+    
+    plt.xlabel('Central scale (angular multipole $\ell$)') # $\pm 1350$')
+    plt.ylabel('CD Score (normalized)') 
