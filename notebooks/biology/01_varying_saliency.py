18b530ea4daf03529fe68dc356289dc4cbc21dc7
Wooseok Ha <haywse@gmail.com>
2021-04-14 17:28:13 +0000

    bio updates

--- notebooks/biology/01_varying_saliency.py
+++ notebooks/biology/01_varying_saliency.py
@@ -13,14 +13,14 @@ if __name__ == '__main__':
         'wave': ['db5'],
         'J': [4],
         'init_factor': [1],
-        'noise_factor': [0.3],
+        'noise_factor': [0],
         'const_factor': [0],
         'batch_size': [100],
         'lr': [0.001],
-        'num_epochs': [50],
+        'num_epochs': [100],
         'attr_methods': ['Saliency'],
-        'lamL1wave': np.round(list(np.geomspace(0.0001, 1, 10)), 5),
-        'lamL1attr': np.round([0] + list(np.geomspace(0.0001, 10, 50)), 5),
+        'lamL1wave': np.round(list(np.geomspace(0.0001, 0.005, 5)), 5),
+        'lamL1attr': np.round([0] + list(np.geomspace(0.0001, 10, 20)), 5),
         'dirname': ['db5_saliency_warmstart_seed=1'],
         'warm_start': [True]
     }
