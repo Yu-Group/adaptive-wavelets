0e2953a052f08b7f28e278bfb3f0174c3fbf1504
Wooseok Ha <haywse@gmail.com>
2021-04-22 12:48:44 +0000

    updates cosmo srange of hyperparams

--- notebooks/cosmology/01_varying_saliency.py
+++ notebooks/cosmology/01_varying_saliency.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
         'lr': [0.001],
         'num_epochs': [30],
         'attr_methods': ['Saliency'],
-        'lamL1wave': np.round(list(np.geomspace(0.0001, 0.005, 5)), 5),
-        'lamL1attr': np.round([0] + list(np.geomspace(0.0001, 10, 20)), 5),
+        'lamL1wave': np.round(list(np.geomspace(0.005, 0.1, 4)), 5),
+        'lamL1attr': np.round([0] + list(np.geomspace(0.001, 10, 20)), 5),
         'dirname': ['db5_saliency_warmstart_seed=1'],
         'warm_start': [True]
     }
