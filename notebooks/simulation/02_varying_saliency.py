ccab60a18c2b8f5a4fe66ab20b9ae4e856bb3652
Wooseok Ha <haywse@gmail.com>
2021-04-29 18:54:30 +0000

    extend lam_attr for symlet

--- notebooks/simulation/02_varying_saliency.py
+++ notebooks/simulation/02_varying_saliency.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         'num_epochs': [50],
         'attr_methods': ['Saliency'],
         'lamL1wave': np.round(list(np.geomspace(0.0001, 0.005, 5)), 5),
-        'lamL1attr': np.round([0] + list(np.geomspace(0.0001, 10, 20)), 5),
+        'lamL1attr': np.round([0] + list(np.geomspace(0.001, 50, 20)), 5),
         'dirname': ['sym5_saliency_warmstart_seed=1'],
         'warm_start': [True]
     }
