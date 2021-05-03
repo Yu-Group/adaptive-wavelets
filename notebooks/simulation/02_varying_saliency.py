b9d00a619de8feadb6e6416f2a076829f4f59473
Wooseok Ha <haywse@gmail.com>
2021-05-03 20:24:37 +0000

    change sym5 sim setting

--- notebooks/simulation/02_varying_saliency.py
+++ notebooks/simulation/02_varying_saliency.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
         'lr': [0.001],
         'num_epochs': [50],
         'attr_methods': ['Saliency'],
-        'lamL1wave': np.round(list(np.geomspace(0.0001, 0.005, 5)), 5),
-        'lamL1attr': np.round([0] + list(np.geomspace(0.001, 50, 20)), 5),
+        'lamL1wave': np.round(list(np.geomspace(0.001, 0.1, 5)), 5),
+        'lamL1attr': np.round([0] + list(np.geomspace(0.001, 100, 20)), 5),
         'dirname': ['sym5_saliency_warmstart_seed=1'],
         'warm_start': [True]
     }
