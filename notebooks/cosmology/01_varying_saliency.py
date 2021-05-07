228f4506e02aba47f80b11bd9018b3ea9ba6ffbe
Wooseok Ha <haywse@gmail.com>
2021-05-07 06:10:58 +0000

    cosmo exp

--- notebooks/cosmology/01_varying_saliency.py
+++ notebooks/cosmology/01_varying_saliency.py
@@ -10,8 +10,9 @@ if __name__ == '__main__':
     
     params_to_vary = {
         'seed': [1],
-        'wave': ['db5'],
+        'wave': ['db3'],
         'J': [4],
+        'mode': ['periodization'],
         'init_factor': [1],
         'noise_factor': [0],
         'const_factor': [0],
@@ -19,9 +20,9 @@ if __name__ == '__main__':
         'lr': [0.001],
         'num_epochs': [50],
         'attr_methods': ['Saliency'],
-        'lamL1wave': np.round(list(np.geomspace(0.005, 0.1, 4)), 5),
-        'lamL1attr': np.round([0] + list(np.geomspace(0.001, 10, 20)), 5),
-        'dirname': ['db5_saliency_warmstart_seed=1'],
+        'lamL1wave': np.round(list(np.geomspace(0.001, 0.1, 5)), 5),
+        'lamL1attr': np.round([0] + list(np.geomspace(0.001, 50, 20)), 5),
+        'dirname': ['db3_saliency_warmstart_mode=per_seed=1'],
         'warm_start': [True]
     }
     ks = sorted(params_to_vary.keys())
