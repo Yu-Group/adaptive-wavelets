91087dd24bc6ea7413ceecca7f9f758c9c4ca218
Wooseok Ha <haywse@gmail.com>
2021-05-07 23:08:05 +0000

    run symlet simulation

--- notebooks/simulation/04_varying_saliency.py
+++ notebooks/simulation/04_varying_saliency.py
@@ -10,18 +10,18 @@ if __name__ == '__main__':
     
     params_to_vary = {
         'seed': [1],
-        'wave': ['coif5'],
+        'wave': ['db5'],
         'J': [4],
-        'init_factor': [1],
+        'init_factor': [0],
         'noise_factor': [0],
-        'const_factor': [0],
+        'const_factor': [0.1],
         'batch_size': [100],
         'lr': [0.001],
         'num_epochs': [50],
         'attr_methods': ['Saliency'],
-        'lamL1wave': np.round(list(np.geomspace(0.0001, 0.005, 5)), 5),
-        'lamL1attr': np.round([0] + list(np.geomspace(0.0001, 10, 20)), 5),
-        'dirname': ['coif5_saliency_warmstart_seed=1'],
+        'lamL1wave': np.round(list(np.geomspace(0.00001, 0.0001, 5)), 5),
+        'lamL1attr': np.round([0] + list(np.geomspace(0.00001, 50, 20)), 5),
+        'dirname': ['const_saliency_warmstart_seed=1'],
         'warm_start': [True]
     }
     ks = sorted(params_to_vary.keys())
